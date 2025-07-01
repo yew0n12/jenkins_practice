f93173c3791f8e162be5512ae235d6d84baa5e66
yew0n12 <164896412+yew0n12@users.noreply.github.com>
2025-07-01 16:23:06 +0900

    Update test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo Hello from the cloned shell script!
+set -e  # 중간에 에러나면 스크립트 종료
+
+echo "배포 스크립트 시작!"
+
+cd /home/jnu/docker-compose-test
+
+echo "기존 컨테이너 종료..."
+docker compose down
+
+echo "새 컨테이너 빌드 후 실행..."
+docker compose up -d --build
+
+echo "배포 완료!"
